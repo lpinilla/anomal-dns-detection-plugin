373d77e998e7bb0942ebc91e45fcb634edcfe1cf
Lautaro Pinilla <lpinilla@itba.edu.ar>
2022-07-09 08:34:44 -0300

    Added more logging to setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,11 @@
 # install dependencies
 apt install -y --no-install-recommends gcc
+echo "Installing dependencies and setup.py"
 # install flare package
 cd  $( dirname -- "$0"; )/flare; pip3 install --no-cache-dir -r requirements.txt && python3 setup.py install
 #copy library to pyenv
 cp -R flare/ /opt/venv/lib/python3.8/site-packages/
 cd ..; pip3 install -r requirements.txt
+echo "Installed successfully, cleaning up"
 # uninstall dependencies
 apt remove -y gcc --purge && apt autoremove -y && apt clean -y
